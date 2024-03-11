63a4a0da19bf0a11c0ad493a474af86a9f572210
macrespo42 <macrespo@student.42.fr>
2024-03-11 17:57:08 +0100

    feat: made fist command

--- src/index.js
+++ src/index.js
@@ -1,8 +1,65 @@
 require("dotenv").config();
+
+const path = require("node:path");
+const fs = require("node:fs");
 const process = require("node:process");
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const folderPath = path.join(__dirname, "commands");
+const commandFolder = fs.readdirSync(folderPath);
+
+for (const folder of commandFolder) {
+  const commandsPath = path.join(folderPath, folder);
+  const commandFile = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of commandFile) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] the command at ${filePath} is missing required "data" or "execute" property`,
+      );
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  console.log(interaction);
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
