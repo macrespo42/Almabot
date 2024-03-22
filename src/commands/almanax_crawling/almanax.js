47427ebecde956349ffc5108a922e4ca6cdfeea2
macrespo42 <macrespo@student.42.fr>
2024-03-22 21:48:43 +0100

    feat: can pick a date with /almanax command

--- src/commands/almanax_crawling/almanax.js
+++ src/commands/almanax_crawling/almanax.js
@@ -1,26 +1,39 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const almanaxOfferings = require("../../../almanaxDB.json");
 
-function getTodayAlmanax() {
-  const today = new Date();
+function getTodayAlmanax(day, month) {
   const todayOffering =
     almanaxOfferings[
-      `${(today.getMonth() + 1).toString().padStart(2, "0")}-${today.getDate().toString().padStart(2, "0")}`
+      `${month.toString().padStart(2, "0")}-${day.toString().padStart(2, "0")}`
     ];
   const offeringEmbed = new EmbedBuilder()
     .setColor(0x0099ff)
-    .setTitle(todayOffering.bonusType)
-    .setDescription(todayOffering.bonus)
-    .setThumbnail(todayOffering?.img)
-    .addFields({ name: "Offrande:", value: todayOffering.offering });
+    .setTitle(todayOffering?.bonusType ?? null)
+    .setDescription(todayOffering?.bonus ?? null)
+    .setThumbnail(todayOffering?.img ?? null)
+    .setImage(todayOffering?.img ?? null)
+    .addFields({
+      name: "Offrande:",
+      value: todayOffering?.offering ?? "Invalid date",
+    });
   return offeringEmbed;
 }
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("almanax")
-    .setDescription("Get the almanax of the day"),
+    .setDescription("Get the almanax of the day")
+    .addIntegerOption((option) =>
+      option.setName("day").setDescription("Day to get offering (Ex: 22)"),
+    )
+    .addIntegerOption((option) =>
+      option.setName("month").setDescription("Month to get offering (Ex: 9)"),
+    ),
   async execute(interaction) {
-    await interaction.reply({ embeds: [getTodayAlmanax()] });
+    const today = new Date();
+    const day = interaction.options.getInteger("day") ?? today.getDate();
+    const month =
+      interaction.options.getInteger("month") ?? today.getMonth() + 1;
+    await interaction.reply({ embeds: [getTodayAlmanax(day, month)] });
   },
 };
