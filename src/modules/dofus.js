745ea7453095aa8c38bff6bcfaa120ea17d55d19
macrespo42 <macrespo@student.42.fr>
2024-09-09 19:13:43 +0200

    fix: relative path

--- src/modules/dofus.js
+++ src/modules/dofus.js
@@ -1,6 +1,6 @@
 const almanaxOfferings = require("../../almanaxDB.json");
 const { getFirsDayOfTheWeek, getTomorrow, getNextMonday } = require("./time");
-const { DAYS } = require("constants.js");
+const { DAYS } = require("./constants.js");
 
 const getAlmanax = (day, month) => {
   const offering =
